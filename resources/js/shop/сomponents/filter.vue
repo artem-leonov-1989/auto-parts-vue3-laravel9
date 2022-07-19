f19e20003039461c134d50b86390077e1bff5d29
Artem Leonov <artem_leonov@hotmail.com>
2022-07-20 01:13:36 +0300

    Обновление

---
+++ resources/js/shop/сomponents/filter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="d-grid gap-2">
+        <button type="button" class="btn btn-outline-primary" v-for="category in categories"
+            :key = category.id
+            @click = 'chooseCategory(category.id)'>
+            {{ category.name }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            categories:[],
+        }
+    },
+    methods: {
+        chooseCategory(id) {
+            console.log(id);
+        }
+    },
+    mounted() {
+        this.$query.get('categories')
+            .then(categories => {
+                this.categories = categories.data.data;
+            })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/shop/сomponents/page.vue
+++ resources/js/shop/сomponents/page.vue
@@ -1,30 +1,43 @@
 <template>
     <nav-bar></nav-bar>
+    <div class="container-fluid mt-3">
+        <div class="row">
+            <div class="col-2">
+                <filter-bar></filter-bar>
+            </div>
+            <div class="col-5">
+                <counter></counter>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 import navBar from "./nav-bar.vue";
+import counter from "./counter.vue";
+import filterBar from "./filter.vue";
 
 export default {
     components: {
         navBar,
+        counter,
+        filterBar,
     },
     data() {
         return {
-            categories: [],
-            parts: [],
+
         }
     },
     methods: {
 
     },
     mounted() {
-        Promise.all([
+        /*Promise.all([
             this.$query.get('categories'),
             this.$query.get('parts')
         ]).then(([categories, parts]) => {
             this.categories = categories.data.data;
             this.parts = parts.data.data;
-        });
+        });*/
     }
 }
 </script>
