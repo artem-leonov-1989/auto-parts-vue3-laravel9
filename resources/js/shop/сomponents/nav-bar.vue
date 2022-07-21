7ec1c66a9dc9bae089282f68c8c9890020ea1c36
Artem Leonov <artem_leonov@hotmail.com>
2022-07-22 00:38:10 +0300

    Завершено выделение выбранных категорий

--- resources/js/shop/сomponents/nav-bar.vue
+++ resources/js/shop/сomponents/nav-bar.vue
@@ -1,27 +1,14 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <router-link to="/" class="nav-link">Магазин</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/" class="nav-link">Оплата</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/" class="nav-link">Обмін</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/" class="nav-link">Контакти</router-link>
-                    </li>
-                </ul>
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container-fluid justify-content-between">
+            <div class="d-flex justify-content-around">
+                <router-link to="/" class="nav-link">Магазин</router-link>
+            </div>
+            <div class="d-flex justify-content-around align-content-center">
+                <router-link to="/" class="nav-link">Кошик</router-link>
+                <login></login>
             </div>
         </div>
-        <login></login>
     </nav>
 </template>
 
